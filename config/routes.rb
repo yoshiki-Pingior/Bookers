c24378359bdb73afed3c8030d705a8df6fd2fd31
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-04-07 09:15:30 +0000

    [add]CSS,format

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'books#top'
-  
+
 end
