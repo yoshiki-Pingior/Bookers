45b7f153d915b35797126bc102cc2e4ae49e008c
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-04-07 07:20:59 +0000

    [add]derete faction

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'books#top'
+  
 end
