aa1a8860d3de8226a9f7ac44460aee9b7e3736c9
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-04-08 04:56:03 +0000

    [Finish]issuees

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,6 +18,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
    if @book.save
+     flash[:notice] = "Book was successfully created."
     redirect_to book_path(@book.id)
    else
      @books = Book.all
@@ -27,15 +28,22 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+     render 'edit'
+    end
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    if  @book.destroy
+      flash[:notice] = "Book was successfully destroied."
     redirect_to books_path
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'books#top'
-
+  root 'books#top'
 end
